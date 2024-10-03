315b13312d6a01ace318cc453e0b50e8d7adb9d9
daniel donofrio <dbrumcassimiro@gmail.com>
2024-10-03 16:36:16 -0300

    criando projeto

---
+++ MySite/src/main/java/entities/Promo.java
@@ -0,0 +1,60 @@
+package entities;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Objects;
+
+public class Promo {
+
+    private long id;
+    private String title;
+    private String url;
+
+    public Promo() {
+    }
+
+    public Promo(long id, String title, String url) {
+        this.id = id;
+        this.title = title;
+        this.url = url;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Promo promo = (Promo) o;
+        return id == promo.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+
+}
